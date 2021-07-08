137e1e524d3231df7f8b992678213057cde95aa4
Paul Anthony McGowan <xhostcom@gmail.com>
2021-07-08 23:01:11 +0100

    Initial commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,12 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  Rails: '@rails/ujs'
+}))
+
+module.exports = environment
