fc545f8445a635392a4bbe394bbf288e29d557a0
Paul Anthony McGowan <xhostcom@gmail.com>
2021-07-10 14:39:31 +0100

    Colors & Buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_073736) do
+ActiveRecord::Schema.define(version: 2021_07_10_110710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2021_07_09_073736) do
     t.datetime "published_at"
     t.string "announcement_type"
     t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
