49801c2e636bd45b0a87b08eff3e53be58747e82
Paul Anthony McGowan <xhostcom@gmail.com>
2021-07-11 22:28:05 +0100

    Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
+  # skip_before_action :authenticate_user!, :only => [:index]
   def index
+    @courses = Course.all.limit(3)
+    @latest_courses = Course.all.limit(3).order(created_at: :desc)
   end
 
   def terms
